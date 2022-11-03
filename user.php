3c959eedd7a03f04c572659fe9ac0dcd68a00d8c
namtrung063 <102019496+namtrung063@users.noreply.github.com>
2022-11-03 11:24:30 +0700

    Add files via upload

--- user.php
+++ user.php
@@ -4,16 +4,17 @@
 #3. Kết nối
 include_once "php/DBConnect.php";
 #4. Thực hiện
-if (isset($_POST['search'])) {
+if(isset($_POST['search']))
+    {
     $valueToSearch = $_POST['valueToSearch'];
-    $query = "SELECT * FROM tbUser_Account WHERE CONCAT(userid, username, fullname, email, phonenumber) LIKE '%" . $valueToSearch . "%'";
+    $query = "SELECT * FROM tbUser_Account WHERE CONCAT(userid, username, fullname, email, phonenumber) LIKE '%".$valueToSearch."%'";
     $search_result = filterTable($query);
     $count = mysqli_num_rows($search_result);
-} else {
+    } else {
     $query = "SELECT * FROM tbUser_Account";
     $search_result = filterTable($query);
     $count = mysqli_num_rows($search_result);
-}
+    }
 
 function filterTable($query)
 {
@@ -23,48 +24,45 @@ function filterTable($query)
 }
 ?>
 <html>
-
-<head>
-    <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>User List</title>
-</head>
-<style>
-    th {
-        text-align: center
-    }
-</style>
-
-<body>
-    <div class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
-        <h2>User List</h2>
-        <hr>
-        <div class="panel-heading">
-        </div>
+    <head>
+        <meta charset="UTF-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <title>User List</title>
+    </head>
+    <style>
+        th{
+            text-align: center
+        }
+    </style>
+    <body>
+        <div class="col-lg-10 col-md-10 col-sm-9 col-xs-12 container">   
+            <h2>User List</h2>
+	<hr style="height:2px;border-width:0;color:gray;background-color:gray">		
         <form action="" method="post">
+            <div class="input-group mb-3">
             <input type="text" name="valueToSearch" placeholder="Value To Search">
             <input type="submit" name="search" value="Search" class="btn btn-success btn-xs"><br>
-
+            </div>
             <?php
-            if (!$count) {
-                echo '<div class = "alert-warning">Records not found!</div>';
-            }
-            ?>
+        if (!$count){
+            echo '<div class = "alert-warning">Records not found!</div>';
+        }
+        ?>
             <hr>
-            <table id="userList" class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Full Name</th>
-                        <th>Email</th>
-                        <th>Phone Number</th>
-                        <th>Address</th>
-                        <th>Delete</th>
-                    </tr>
-                    <?php
-                    while ($field = mysqli_fetch_array($search_result)) :
-                    ?>
+	<table id="userList" class="table table-bordered table-striped">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Username</th>
+				<th>Full Name</th>
+				<th>Email</th>
+				<th>Phone Number</th>
+				<th>Address</th>					
+				<th>Status</th>
+			</tr>
+                        <?php
+                            while($field = mysqli_fetch_array($search_result)):
+                        ?>
                         <tr>
                             <td><?= $field[0] ?></td>
                             <td><?= $field[1] ?></td>
@@ -73,26 +71,34 @@ function filterTable($query)
                             <td><?= $field[5] ?></td>
                             <td>
                                 <?php
-                                $query1 = "SELECT * FROM tbDelivery_Address WHERE userid = '$field[0]'";
-                                $rs1 = mysqli_query($conn, $query1);
-                                while ($field1 = mysqli_fetch_array($rs1)) :
+                                    $query1 = "SELECT * FROM tbDelivery_Address WHERE userid = '$field[0]' AND is_default = 1";
+                                    $rs1 = mysqli_query($conn, $query1);
+                                    while ($field1 = mysqli_fetch_array($rs1)) :
                                 ?>
                                     <?= $field1[2] ?> <br>
                                 <?php
-                                endwhile;
+                                    endwhile;
                                 ?>
                             </td>
                             <td style="text-align: center">
-                                <a href="php/deleteUser.php?code=<?= $field[0]; ?>" onclick="return confirm('Really want to delete <?= $field[1] ?> ???')">Delete</a>
+                                <a style="text-decoration: none; color: black" href="blockuser.php?code=<?= $field[0]; ?>"
+                                    onclick="return confirm('Really want to switch status of <?= $field[1] ?> ???')">
+                                <?php
+                                if ($field[6]):
+                                    echo '<strong style="color: green">Active</strong>';
+                                else :
+                                    echo '<strong style="color: red">Inactive</strong>';
+                                endif;
+                                ?>
+                                </a>
                             </td>
                         </tr>
-                    <?php
-                    endwhile;
-                    ?>
-        </form>
-        </thead>
-        </table>
-    </div>
-</body>
-
+                        <?php
+                            endwhile;
+                        ?>
+                        </form>
+		</thead>
+	</table>
+</div>
+    </body>
 </html>
